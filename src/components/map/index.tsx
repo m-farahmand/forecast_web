0f1430961ecb78ce0f210b24a0af6904d0f7aa51
farahmand <m.farahmandzad@outlook.com>
2021-08-22 23:05:28 +0430

    fix(style):Change
    Change leaflet style

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,6 +1,7 @@
 import { FunctionalComponent, h } from 'preact';
 import { useReducer } from 'preact/hooks';
 import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet'
+import style from './style.css';
 
 interface MapConfig {
   lat: number;
@@ -22,7 +23,7 @@ const MapComponent: FunctionalComponent = () => {
   // const { config, setConfig } = useReducer(setConfigCallback, presetConfig);
   const position = [51.505, -0.09];
   return (
-    <MapContainer center={position} zoom={13}>
+    <MapContainer class={style.leafletcontainer} center={position} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
